247e6b7c53612ec7207ea30baa0cd74a55f6a770
Md. Abdullah Al Nafiz <alnafiz000@gmail.com>
2026-03-15 16:46:35 +0600

    first commit

---
+++ CWH/Chapter1/first.py
@@ -0,0 +1,5 @@
+print("Hello Bangladesh")
+# lungi shamla
+"""
+This is just a test file to check the git commands and how to use them. I am learning git and github for version control and collaboration. This file will be used to practice committing changes, pushing to remote repositories, and managing branches. I will also learn how to resolve merge conflicts and use pull requests for code reviews. Overall, this is a great way to get hands-on experience with git and improve my workflow as a developer.
+"""
