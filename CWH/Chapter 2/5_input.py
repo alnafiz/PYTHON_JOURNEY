b3a2b5290e1d453b45f004b15cc81d8295d07d0b
Md.Abdullah AL Nafiz <alnafiz000@gmail.com>
2026-03-19 23:05:00 +0600

    not so important

--- CWH/Chapter 2/5_input.py
+++ CWH/Chapter 2/5_input.py
@@ -1,3 +1,8 @@
-a=input("Enter your roll:")
+# a=input("Enter your roll:")
 
-print("Your roll is "+a)
+# print("Your roll is "+a)
+
+x=input("Enter your name:")
+y=input("Enter your  wife name:")
+
+print("THe couple name is :"+x+y)
