c35caa28620dbfc457cf9b2c00423c849496d6a0
Md.Abdullah AL Nafiz <alnafiz000@gmail.com>
2026-05-05 01:05:51 +0600

    basic string learned fromm  aapna college

---
+++ CWH/strings/lectture_two.py
@@ -0,0 +1,25 @@
+str1="This is a string"
+str2='This is also a string'
+# print(str1)
+
+# str3="eta ager line.\nAr eta porer line"
+
+# print(str3)
+
+# str4="Gap er agey \t gap er pore "
+# print(str4)
+
+#concataination
+# final_str=str1+str2
+# print(final_str)
+
+# #length
+# len1=len(str1)
+# print(len1)
+
+#iindexing
+
+str="Joy bangla"
+ch=str[2]
+
+print(ch)
---
+++ OOPS/instance.py
@@ -0,0 +1,23 @@
+# class Student:
+#     name="Al Nafiz"
+
+#     #creating object
+
+    
+# s1=Student()
+   
+# print(s1.name)
+
+# s2=Student()
+# print(s2.name)
+
+
+class Car:
+    color="Pink"
+    brand="BMW"
+
+c1=Car()
+print("The colour you chosen is:",c1.color)
+
+c2=Car()
+print("Brand:",c2.brand)
