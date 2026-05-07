d82f832d9861922a5e9dc3476635d309d93e0757
Md.Abdullah AL Nafiz <alnafiz000@gmail.com>
2026-05-08 00:08:46 +0600

    length and counting related problems practiced

--- strings/problems _solving/string_count.py
+++ strings/problems _solving/string_count.py
@@ -1,4 +1,8 @@
 #wap to take user name as input and count the number of characters in the name
 
-name=input("Enter yoour userrname:")
-print("The numbers of  your name characters are: ",len(name))
+# name=input("Enter yoour userrname:")
+# print("The numbers of  your name characters are: ",len(name))
+
+#wap to find the occurence of $ in a string
+str="$i $am a & sign and $ is my baby $99.99 percent $blah blah$"
+print("The total occurence of $ are :",str.count("$"))   
