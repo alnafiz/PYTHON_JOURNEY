193712ac45f6b68b3316e7c11cbcd9b179283627
Md.Abdullah AL Nafiz <alnafiz000@gmail.com>
2026-05-07 23:51:16 +0600

    learned basics of python string

---
+++ strings/string_func.py
@@ -0,0 +1,16 @@
+#endswith function
+str="honour connoor  donnor lonnor "
+# print(str.endswith("er"))
+
+#str.capitalize
+# print(str.capitalize())
+
+#string replace
+# print(str.replace("o","a"))
+# print(str)
+
+#string find
+# print(str.find("r"))
+
+#string count
+print(str.count("r"))
