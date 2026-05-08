7f80d24898c7decf603ee0e87dca5477163b4a13
Md.Abdullah AL Nafiz <alnafiz000@gmail.com>
2026-05-08 17:27:36 +0600

    learne mos used  methods

---
+++ lists/list_method.py
@@ -0,0 +1,18 @@
+list=[0,1,2,3,4,5,-2,-1,6]
+list.append(11)
+
+print(list)
+
+# list.sort()
+# print("After sorting:")
+# print(list)
+
+# list.sort(reverse=True)
+# print("After reverse sorting:")
+# print(list)
+
+list.insert(1,-20)
+print(list)
+
+list.pop(3)
+print(list)
