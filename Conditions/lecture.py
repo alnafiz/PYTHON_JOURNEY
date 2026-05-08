61daa9d4dbfa75164b29c7a25e978800e47bc936
Md.Abdullah AL Nafiz <alnafiz000@gmail.com>
2026-05-08 17:35:29 +0600

    basic intro of conditionn

---
+++ Conditions/lecture.py
@@ -0,0 +1,9 @@
+age=int(input("Anna ,please enter your age:"))
+if(age >= 18):
+   print("Anna congrats,you can  go to bar now and drink beer")
+
+elif(age >= 16 and age < 18):
+   print("Anna just wait some more time")
+    0o
+else:
+   print("Oy tor ki boyosh hoise?")
