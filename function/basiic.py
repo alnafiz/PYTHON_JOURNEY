c2b92148cc12dee2eb7b5ffe076af98f55370af1
Md.Abdullah AL Nafiz <alnafiz000@gmail.com>
2026-05-10 21:26:06 +0600

    sum function

--- function/basiic.py
+++ function/basiic.py
@@ -1,4 +1,19 @@
-def joy():
-    print("This is fake :")
+# def joy():
+#     print("This is fake :")
 
-joy()
+# joy()
+
+# a=4
+# b=7
+# sum=a+b
+
+# print(sum)
+
+def calc_sum(a,b):
+    sum=a+b
+    print(sum)
+    return sum
+
+calc_sum(7,8)
+calc_sum(9,18)
+calc_sum(12,88)
