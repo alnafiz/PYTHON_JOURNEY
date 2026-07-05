89721693dd888abdbcfde81703e854f34a1f666a
Md.Abdullah AL Nafiz <alnafiz000@gmail.com>
2026-07-05 23:57:06 +0600

    one  more practice  of list

---
+++ Professional_Track/lists & tuples/list practice/list_sort.py
@@ -0,0 +1,9 @@
+scores = [45, 12, 89, 5, 67, 23]
+
+# ছোট থেকে বড় ক্রমে সাজানো (.sort() ব্যবহার করে)
+scores.sort()
+print("ছোট থেকে বড় ক্রমে:", scores)
+
+# বড় থেকে ছোট ক্রমে সাজানো (reverse=True ব্যবহার করে)
+scores.sort(reverse=True)
+print("বড় থেকে ছোট ক্রমে:", scores)
