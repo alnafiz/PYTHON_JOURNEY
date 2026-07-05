f1d67b864fe10e967f908068f5ae364508e0a272
Md.Abdullah AL Nafiz <alnafiz000@gmail.com>
2026-07-05 19:13:43 +0600

    revised string and praccticed some basic problems of string

---
+++ Professional_Track/strings & conditional statements/string practice/reverse_slicing.py
@@ -0,0 +1,5 @@
+name="NAZIFA"
+reversed_text=name[::-1]
+
+print("name:",name)
+print("reversed_name:",reversed_text)
---
+++ Professional_Track/strings & conditional statements/sttringgggg.py
@@ -0,0 +1,31 @@
+# str1="This is a string inside double quote."
+# str2='This ia also a string inside single quote '
+# str3="This is first line \n This is second line"
+
+# # print(str1)
+# # print(str2)
+# print(str3)
+
+
+
+# #concatination
+# str1="Hey"
+# str2="Baby"
+# final_str=str1+str2
+# print(final_str)
+
+
+
+# #length
+# str="HALLO"
+# print(len(str))
+
+
+##indexing
+# str="Bangladesh Zindabaad"
+# ch=str[3]
+# print(ch)
+
+##slicing
+# str="Bangladesh Zindabaad"
+# print(str[:4])
