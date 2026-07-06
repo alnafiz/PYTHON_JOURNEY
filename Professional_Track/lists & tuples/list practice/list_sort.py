3e96618dfd4a1f251ac49d3bc6c77d907b1d9754
Md.Abdullah AL Nafiz <alnafiz000@gmail.com>
2026-07-06 23:15:02 +0600

    revised the loop basics

--- Professional_Track/lists & tuples/list practice/list_sort.py
+++ Professional_Track/lists & tuples/list practice/list_sort.py
@@ -1,9 +1,8 @@
 scores = [45, 12, 89, 5, 67, 23]
 
-# ছোট থেকে বড় ক্রমে সাজানো (.sort() ব্যবহার করে)
 scores.sort()
-print("ছোট থেকে বড় ক্রমে:", scores)
+print("from small to big:", scores)
 
-# বড় থেকে ছোট ক্রমে সাজানো (reverse=True ব্যবহার করে)
+
 scores.sort(reverse=True)
-print("বড় থেকে ছোট ক্রমে:", scores)
+print("from big to small:", scores)
---
+++ Professional_Track/loops/loops.py
@@ -0,0 +1,9 @@
+# fruits = ["Apple", "Banana", "Cherry"]
+
+
+# for f in fruits:
+#     print(f)
+
+
+for i in range(5):
+    print(f"loop running: {i}")
