5a9a3821a18f73c5a9be6aa8e2965e8a480f79ab
Md.Abdullah AL Nafiz <alnafiz000@gmail.com>
2026-07-12 11:20:09 +0600

    started oop

--- Professional_Track/function and recursion/function.py
+++ Professional_Track/function and recursion/function.py
@@ -2,9 +2,8 @@
 
 def calc_sum(a,b):
     sum=a+b
-    print(sum)
     return sum
+ 
 
-a=int(input("enter a:"))
-b=int(input("enter b:"))
-calc_sum(a,b)
+sum=calc_sum(5,6)
+print(sum)
---
+++ ooop/lec_8.py
@@ -0,0 +1,7 @@
+class Student:
+    name ="abdullah"
+
+s1=Student()
+print(s1.name)
+s2=Student()
+print(s2.name)
